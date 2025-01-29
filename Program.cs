9a009ca9e098fb19b61f26e14eae0fdbd077bbd6
uboston <uboston@byu.edu>
2025-01-29 10:02:24 -0700

    Loops until valid input

--- Program.cs
+++ Program.cs
@@ -33,33 +33,41 @@ public class Program
             // Players 1 makes their move
             if (playerone)
             {
+                bool validMove = false;
                 // Asks for player one input
+                Console.WriteLine(' ');
                 Console.WriteLine("Player one place an 'X' (e.g. 1)");
                 
                 // Call the print board method 
                 gb.PrintBoard(board);
-                char playerOneChoice = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-                
-                // Makes sure their choice is an available option
-                if (int.TryParse(playerOneChoice.ToString(), out int playerOneChoiceInt) && playerOneChoiceInt >= 1 &&
-                    playerOneChoiceInt <= 9)
+                while (!validMove)
                 {
-                    int row = (playerOneChoiceInt - 1) / 3;
-                    int col = (playerOneChoiceInt - 1) % 3;
+                    char playerOneChoice = Console.ReadKey().KeyChar;
+                    Console.WriteLine(' ');
 
-                    if (board[row, col] != 'X' && board[row, col] != 'O')
+                    // Makes sure their choice is an available option
+                    if (int.TryParse(playerOneChoice.ToString(), out int playerOneChoiceInt) &&
+                        playerOneChoiceInt >= 1 &&
+                        playerOneChoiceInt <= 9)
                     {
-                        board[row, col] = 'X';
+                        int row = (playerOneChoiceInt - 1) / 3;
+                        int col = (playerOneChoiceInt - 1) % 3;
+
+                        if (board[row, col] != 'X' && board[row, col] != 'O')
+                        {
+                            board[row, col] = 'X';
+                            validMove = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("This spot is already taken. Try again");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("This spot is already taken");
+                        Console.WriteLine("Invalid input. Please choose an open spot number (1-9)");
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please choose an open spot number (1-9)");
+
                 }
 
                 // Call CheckWinner Method
@@ -68,12 +76,14 @@ public class Program
                 // Checks results and ends game if someone won
                 if (result == 'X')
                 {
+                    Console.WriteLine(' ');
                     Console.WriteLine("Player one won!");
                     gb.PrintBoard(board);
                     endGame = true;
                 }
                 else if (result == 'C')
                 {
+                    Console.WriteLine(' ');
                     Console.WriteLine("Game ended in a tie! Better luck next time!");
                     gb.PrintBoard(board);
                     endGame = true;
@@ -81,7 +91,6 @@ public class Program
                 else
                 {
                     Console.WriteLine("Good choice!");
-                    gb.PrintBoard(board);
                 }
                 playerone = false;
             }
@@ -89,31 +98,40 @@ public class Program
             // Switches to player 2's turn and does the exact same thing as player 1's turn
             else
             {
+                bool validMove = false;
+                Console.WriteLine(' ');
                 Console.WriteLine("Player two place an 'O' (e.g. 1)");
                 
                 // Call the print board method 
                 gb.PrintBoard(board);
-                char playerTwoChoice = Console.ReadKey().KeyChar;
-                
-                // Checks for errors in the users answer
-                if (int.TryParse(playerTwoChoice.ToString(), out int playerTwoChoiceInt) && playerTwoChoiceInt >= 1 &&
-                    playerTwoChoiceInt <= 9)
+                while (!validMove)
                 {
-                    int row = (playerTwoChoiceInt - 1) / 3;
-                    int col = (playerTwoChoiceInt - 1) % 3;
+                    char playerTwoChoice = Console.ReadKey().KeyChar;
+                    Console.WriteLine(' ');
 
-                    if (board[row, col] != 'X' && board[row, col] != 'O')
+
+                    // Checks for errors in the users answer
+                    if (int.TryParse(playerTwoChoice.ToString(), out int playerTwoChoiceInt) &&
+                        playerTwoChoiceInt >= 1 &&
+                        playerTwoChoiceInt <= 9)
                     {
-                        board[row, col] = 'O';
+                        int row = (playerTwoChoiceInt - 1) / 3;
+                        int col = (playerTwoChoiceInt - 1) % 3;
+
+                        if (board[row, col] != 'X' && board[row, col] != 'O')
+                        {
+                            board[row, col] = 'O';
+                            validMove = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("This spot is already taken");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("This spot is already taken");
+                        Console.WriteLine("Invalid input. Please choose an open spot number (1-9)");
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please choose an open spot number (1-9)");
                 }
 
                 // Call CheckWinner Method
@@ -122,12 +140,14 @@ public class Program
                 // Checks for results and ends game if someone won
                 if (result == 'O')
                 {
+                    Console.WriteLine(' ');
                     Console.WriteLine("Player two won!");
                     gb.PrintBoard(board);
                     endGame = true;
                 }
                 else if (result == 'C')
                 {
+                    Console.WriteLine(' ');
                     Console.WriteLine("Game ended in a tie! Better luck next time!");
                     gb.PrintBoard(board);
                     endGame = true;
@@ -135,7 +155,6 @@ public class Program
                 else
                 {
                     Console.WriteLine("Good choice!");
-                    gb.PrintBoard(board);
                 }
                 playerone = true;
             }   
